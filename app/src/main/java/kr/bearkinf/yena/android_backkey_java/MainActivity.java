1e2ee786491a9c8db9d12098c9c6ae072434675e
BearKinf <bearkinf@gmail.com>
2018-07-20 14:21:45 +0900

    Initial commit

---
+++ app/src/main/java/kr/bearkinf/yena/android_backkey_java/MainActivity.java
@@ -0,0 +1,32 @@
+package kr.bearkinf.yena.android_backkey_java;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import kr.bearkinf.yena.backkey_library_java.BackKeyFinishHandler;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    BackKeyFinishHandler backKeyFinishHandler;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        backKeyFinishHandler = new BackKeyFinishHandler(this);
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        // super.onBackPressed();
+        backKeyFinishHandler.onBackPressed();
+
+
+    }
+}
---
+++ backkey_library_java/src/main/java/kr/bearkinf/yena/backkey_library_java/BackKeyFinishHandler.java
@@ -0,0 +1,52 @@
+package kr.bearkinf.yena.backkey_library_java;
+
+import android.app.Activity;
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by jojaeyeong-pc on 2018. 4. 17..
+ */
+
+public class BackKeyFinishHandler {
+
+    private long endTime = 2000;
+
+    private long backKeyFinishTime = 0;
+
+    private Context mContext;
+
+    private Toast mToast;
+
+    public BackKeyFinishHandler(Context context) {
+        this.mContext = context;
+    }
+
+    public BackKeyFinishHandler(Context context, long endTime) {
+        this(context);
+        this.endTime = endTime;
+    }
+
+    public void onBackPressed() {
+
+        if (System.currentTimeMillis() > backKeyFinishTime + endTime) {
+            backKeyFinishTime = System.currentTimeMillis();
+            showToast();
+            return;
+        }
+
+        if (System.currentTimeMillis() <= backKeyFinishTime + endTime) {
+            mToast.cancel();
+            ((Activity) mContext).finish();
+        }
+    }
+
+
+    private void showToast() {
+        mToast = Toast.makeText(mContext, "\'뒤로\'버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT);
+        mToast.show();
+
+
+    }
+
+}
